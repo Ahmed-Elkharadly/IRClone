4b534acd96120d6c275e14921c91b75e90bd3c99
Ahmed-Elkharadly <ahmed.ehab@argaam.com>
2023-11-20 14:27:33 +0200

    handle swr

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -36,7 +36,7 @@ export function register(config) {
 
     window.addEventListener("load", () => {
       const swUrl = process.env.PUBLIC_URL
-        ? `https://ir-clone.vercel.app/service-worker.js`
+        ? `${process.env.PUBLIC_URL}/service-worker.js`
         : "/service-worker.js";
 
       if (isLocalhost) {
